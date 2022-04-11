ccdc23e4a6f3ba9d3d3c5e6c3828889f96e47bcd
Logan Parke <logan.c.parke@gmail.com>
2022-04-11 12:28:53 -0700

    13.2 finished

--- config/connection.js
+++ config/connection.js
@@ -1,10 +1,9 @@
-// import the Sequelize constructor from the library
 const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-// create connection to our database, pass in your MySQL information for username and password
-const sequelize = new Sequelize('just_tech_news_db', 'root', 'QpSKLA5%SQL', {
+// create connection to our db
+const sequelize = new Sequelize(process.env.DB_NAME, 'root', 'QpSKLA5%SQL', {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
--- models/User.js
+++ models/User.js
@@ -1,11 +1,16 @@
 const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../config/connection');
 const bcrypt = require('bcrypt');
+const sequelize = require('../config/connection');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
-// define table columns and configuration
+// create fields/columns for User model
 User.init(
   {
     id: {
@@ -39,12 +44,12 @@ User.init(
       // set up beforeCreate lifecycle "hook" functionality
       async beforeCreate(newUserData) {
         newUserData.password = await bcrypt.hash(newUserData.password, 10);
-          return newUserData;
+        return newUserData;
       },
-      // set up beforeUpdate lifecycle "hook" functionality
-      async beforeUpdate(updateduserData) {
-        updateduserData.password = await bcrypt.hash(updateduserData.password, 10);
-        return updateduserData;
+
+      async beforeUpdate(updatedUserData) {
+        updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+        return updatedUserData;
       }
     },
     sequelize,
